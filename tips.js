f4719c9059450b3d0e371ed3a103880c064ced2a
Lucas Chinelate <lucaschinelate@hotmail.com>
2024-06-26 14:03:29 -0300

    modificacoes

---
+++ tips.js
@@ -0,0 +1,14 @@
+﻿const tips = [
+    "Invista em peças que realcem suas melhores características.",
+    "Conforto é tão importante quanto estilo.",
+    "Não tenha medo de experimentar novos estilos e cores."
+];
+
+let currentTip = 0;
+
+document.getElementById('nextTip').addEventListener('click', function() {
+    currentTip = (currentTip + 1) % tips.length; // Isso fará o loop das dicas
+    document.getElementById('tipText').textContent = tips[currentTip];
+});
+
+document.getElementById('tipText').textContent = tips[currentTip]; // Inicia com a primeira dica
