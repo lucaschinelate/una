2b224c747a1fbf11fcc2927ed9f233df18673e0e
Lucas Chinelate <lucaschinelate@hotmail.com>
2024-06-26 16:00:29 -0300

    teste

--- navbar.js
+++ navbar.js
@@ -1,11 +1,12 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
+  // HTML para o navbar
   var navbarHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="user-profile d-flex align-items-center gap-3">
             <img src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" alt="Ícone do usuário" class="user-icon rounded-circle">
             <div>
                 <h3 class="mb-0">Angelita</h3>
-                <p class="text-muted mb-0">angelitadugoni@icloud.coms</p>
+                <p class="text-muted mb-0">angelitadugoni@icloud.com</p>
             </div>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,4 +23,11 @@
         </div>
     </nav>`;
   document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+
+  // HTML para o footer
+  var footerHTML = `
+    <footer class="footer py-3 bg-light text-center">
+        <span>&copy; 2024 Croqui-me. Todos os direitos reservados.</span>
+    </footer>`;
+  document.body.insertAdjacentHTML('beforeend', footerHTML);
 });
