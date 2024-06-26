c123edc5cfaf81507ab3bdab23c59b2fe4ce50e7
Lucas Chinelate <lucaschinelate@hotmail.com>
2024-06-26 15:45:58 -0300

    teste

--- navbar.js
+++ navbar.js
@@ -1,9 +1,13 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
   var navbarHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-            <img src="imagens/logo_min.png" alt="Croqui-me" class="logo"> Croqui-me
-        </a>
+        <div class="user-profile d-flex align-items-center gap-3">
+            <img src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" alt="Ícone do usuário" class="user-icon rounded-circle">
+            <div>
+                <h3 class="mb-0">Angelita</h3>
+                <p class="text-muted mb-0">angelitadugoni@icloud.com</p>
+            </div>
+        </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
