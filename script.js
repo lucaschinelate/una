679c2d14f80a1966dca570fb4994a6235a9df150
Lucas <lucaschinelate@hotmail.com>
2024-06-27 02:09:03 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById('toggleTips');
     const tipText = document.getElementById('tipText');
+    const line = document.getElementById('line');
     const nextTipButton = document.getElementById('nextTip');
     const toggleIcon = document.getElementById('toggleIcon');
 
@@ -8,10 +9,12 @@
         // Toggle visibility of tip text and next tip button
         if (tipText.style.display === 'none') {
             tipText.style.display = '';
+            line.style.display = '';
             nextTipButton.style.display = '';
             toggleIcon.className = 'fas fa-minus';
         } else {
             tipText.style.display = 'none';
+            line.style.display = 'none';
             nextTipButton.style.display = 'none';
             toggleIcon.className = 'fas fa-plus';
         }
